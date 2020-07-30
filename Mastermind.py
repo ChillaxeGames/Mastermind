da35a8cf9cc0be3e9635c12022f5cfd6c96b6955
ChillaxeGames <39731442+ChillaxeGames@users.noreply.github.com>
2020-07-30 22:49:35 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -46,6 +46,8 @@ def DrawTable(B, H):
   
 
 
+
+
 def SinglePlayer():
   os.system('cls')
   print()
@@ -197,6 +199,252 @@ def SinglePlayer():
   print()
   stop = input("Press Enter To Continue")
 
+
+
+
+
+def TwoPlayer():
+  os.system('cls')
+  print()
+  print("88888888888                          8888888b.  888                                    ")
+  print("    888                              888   Y88b 888                                    ")
+  print("    888                              888    888 888                                    ")
+  print("    888  888  888  888  .d88b.       888   d88P 888  8888b.  888  888  .d88b.  888d888 ")
+  print("    888  888  888  888 d88  88b      8888888P   888      88b 888  888 d8P  Y8b 888P    ")
+  print("    888  888  888  888 888  888      888        888 .d888888 888  888 88888888 888     ")
+  print("    888  Y88b 888 d88P Y88..88P      888        888 888  888 Y88b 888 Y8b.     888     ")
+  print("    888    Y8888888P     Y88P        888        888  Y888888   Y88888   Y8888  888     ")
+  print("                                                                  888                  ")
+  print("                                                             Y8b d88P                  ")
+  print("                                                               Y88P ")
+  print()
+  print("You have chosen " + Fore.BLUE + " two player mode")
+  print("This mode requires 2 people to play, and operates much in the style of traditional Mastermind > where your points are determined by the number of turns it takes for the other player to crack your code.")
+  print("You will take turns being " + Fore.YELLOW + "Codemaker" + Fore.RESET + " and " + Fore.YELLOW + "Codebreaker")
+  print()
+  user1Name = input("Enter the name of Player 1 > ")
+  user2Name = input("Enter the name of Player 2 > ")
+  print()
+  valid = 0
+  while(valid == 0):
+    try:
+      rounds = int(input("Enter how many complete rounds you wish to play (A complete round consists of 2 rounds, in which both users play as " + Fore.YELLOW + "Codemaker" + Fore.RESET + ") > "))
+      valid = 1
+    except ValueError:
+      print("Enter a " + Fore.RED + "number" + Fore.RESET + " value")
+    print()
+  print()
+  os.system('cls')
+  print()
+  roundCounter = 1
+  user1Points = 0
+  user2Points = 0
+  codemaker = 1
+  # Symbols > ◯⬤🞉
+  # Main Peg Colours 0 = none, 1 = red, 2 = green, 3 = blue, 4 = cyan, 5 = magenta, 6 = yellow
+  # Hint Peg Colours 0 = none, 1 = black, 2 = white
+  for i in (rounds * 2 * "."):
+    B = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    H = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    Colours = [Fore.RED + "⬤ ", Fore.GREEN + "⬤ ", Fore.BLUE + "⬤ ", Fore.CYAN + "⬤ ", Fore.MAGENTA + "⬤ ", Fore.YELLOW + "⬤ ", ]
+    winState = 0
+    turnsleft = 12
+    print()
+    print()
+    print(Fore.GREEN + "ROUND " + str(roundCounter))
+    if(codemaker == 1):
+      print(user1Name + " is the " + Fore.YELLOW + " Codemaker")
+    else:
+      print(user2Name + " is the " + Fore.YELLOW + " Codemaker")
+    print()
+    print(Fore.YELLOW + "Codemaker's " + Fore.RESET + "turn.")
+    print()
+    print("1 > " + Fore.RED + "⬤" + Fore.RESET + "   ,   2 > " + Fore.GREEN + "⬤")
+    print("3 > " + Fore.BLUE + "⬤" + Fore.RESET + "   ,   4 > " + Fore.CYAN + "⬤")
+    print("5 > " + Fore.MAGENTA + "⬤" + Fore.RESET + "   ,   6 > " + Fore.YELLOW + "⬤")
+    print()
+    Answer = [0,0,0,0]
+    entryComplete = 0
+    while(entryComplete == 0):
+      index = 0
+      sequence = ""
+      for j in "....":
+        valid = 0
+        while(valid == 0):
+          try:
+            entry = int(input("Enter the number for space " + str(index) + " > "))
+            if(entry < 7 and entry > 0):
+              valid = 1
+              Answer[index] = entry
+              sequence = sequence + Colours[entry - 1] + " "
+              print(sequence)
+            else:
+              print("Enter a number between " + Fore.RED + "1 and 6")
+          except ValueError:
+            print("Enter a " + Fore.RED + "number" + Fore.RESET + " value")
+        index = index + 1
+      confirm = input("Confirm Selection? (y/n) > ")
+      if(confirm == "y" or confirm == "Y"):
+        entryComplete = 1
+      else:
+        print()
+    print("You are the " + Fore.YELLOW + " Codebreaker")
+    roundCounter = roundCounter + 1
+    turn = 2
+    while(turnsleft > 0):
+      if(turn == 2):
+        for m in ".........":
+          print()
+        os.system('cls')
+        print()
+        if(codemaker == 1):
+          print(Fore.YELLOW + user2Name + "'s " + Fore.RESET + "turn.")
+        else:
+          print(Fore.YELLOW + user1Name + "'s " + Fore.RESET + "turn.")
+        print()
+        print()
+        DrawTable(B, H)
+        print("_________________")
+        print("| ? | ? | ? | ? |")
+        print("-----------------")
+        print()
+        print()
+        attempt = [0,0,0,0]
+        print("1 > " + Fore.RED + "⬤" + Fore.RESET + "   ,   2 > " + Fore.GREEN + "⬤")
+        print("3 > " + Fore.BLUE + "⬤" + Fore.RESET + "   ,   4 > " + Fore.CYAN + "⬤")
+        print("5 > " + Fore.MAGENTA + "⬤" + Fore.RESET + "   ,   6 > " + Fore.YELLOW + "⬤")
+        print()
+        entryComplete = 0
+        while(entryComplete == 0):
+          index = 0
+          sequence = ""
+          for j in "....":
+            valid = 0
+            while(valid == 0):
+              try:
+                entry = int(input("Enter the number for space " + str(index) + " > "))
+                if(entry < 7 and entry > 0):
+                  valid = 1
+                  attempt[index] = entry
+                  sequence = sequence + Colours[entry - 1] + " "
+                  print(sequence)
+                else:
+                  print("Enter a number between " + Fore.RED + "1 and 6")
+              except ValueError:
+                print("Enter a " + Fore.RED + "number" + Fore.RESET + " value")
+            index = index + 1
+          confirm = input("Confirm Selection? (y/n) > ")
+          if(confirm == "y" or confirm == "Y"):
+            entryComplete = 1
+          else:
+            print()
+        print()
+        B[12 - turnsleft] = attempt
+        turn = 1
+      else:
+        for m in ".........":
+          print()
+        os.system('cls')
+        print()
+        if(codemaker == 1):
+          print(Fore.YELLOW + user1Name + "'s " + Fore.RESET + "turn.")
+        else:
+          print(Fore.YELLOW + user2Name + "'s " + Fore.RESET + "turn.")
+        print()
+        stop = input("Press enter once only the " + Fore.YELLOW + " Codemaker" + Fore.RESET + " can see the screen.")
+        print()
+        print()
+        print()
+        pegs = [0, 0, 0, 0]
+        pegColours = [" ", Fore.BLACK + "🞉 ", Fore.WHITE + "🞉 "]
+        entryComplete = 0
+        print("Answer              > " + Colours[Answer[0] - 1] + " " + Colours[Answer[1] - 1] + " " + Colours[Answer[2] - 1] + " " + Colours[Answer[3] - 1])
+        print()
+        print(Fore.YELLOW + "Codebreaker's" + Fore.RESET + " guess > " + Colours[B[12 - turnsleft][0] - 1] + " " + Colours[B[12 - turnsleft][1] - 1] + " " + Colours[B[12 - turnsleft][2] - 1] + " " + Colours[B[12 - turnsleft][3] - 1])
+        print()
+        print()
+        print("0 > No Peg")
+        print("1 > " + Fore.BLACK + "🞉" + Fore.RESET + "   (Correctly coloured peg in the right place)")
+        print("2 > " + Fore.WHITE + "🞉" + Fore.RESET + "   (Correctly coloured peg in the wrong place)")
+        while(entryComplete == 0):
+          index = 0
+          sequence = ""
+          for j in "....":
+            valid = 0
+            while(valid == 0):
+              try:
+                entry = int(input("Enter the number for space " + str(index) + " > "))
+                if(entry < 3 and entry > -1):
+                  valid = 1
+                  pegs[index] = entry
+                  sequence = sequence + pegColours[entry] + " "
+                  print(sequence)
+                else:
+                  print("Enter a number between " + Fore.RED + "0 and 2")
+              except ValueError:
+                print("Enter a " + Fore.RED + "number" + Fore.RESET + " value")
+            index = index + 1
+          confirm = input("Confirm Selection? (y/n) > ")
+          if(confirm == "y" or confirm == "Y"):
+            entryComplete = 1
+          else:
+            print()
+        H[12 - turnsleft] = pegs
+        if(pegs == [1, 1, 1, 1]):
+          turnsleft = 0
+          winState = 1
+        else:
+          turnsleft = turnsleft - 1
+          turn = 2
+        if(codemaker == 1):
+          user1Points = user1Points + 1
+        else:
+          user2Points = user2Points + 1
+        print() 
+        print()
+    if(winState == 0):
+      DrawTable(B, H)
+      print("_________________")
+      print("| " + Colours[Answer[0] - 1] + Fore.RESET + "| " + Colours[Answer[1] - 1] + Fore.RESET + "| " + Colours[Answer[2] - 1] + Fore.RESET + "| " + Colours[Answer[3] - 1] + Fore.RESET + "|")
+      print("-----------------")
+      print()
+      print()
+      print(Fore.YELLOW + "Codemaker" + Fore.RESET + " wins - No Turns Left")
+      if(codemaker == 1):
+        user1Points = user1Points + 1
+      else:
+        user2Points = user2Points + 1
+    else:
+      DrawTable(B, H)
+      print("_________________")
+      print("| " + Colours[Answer[0] - 1] + Fore.RESET + "| " + Colours[Answer[1] - 1] + Fore.RESET + "| " + Colours[Answer[2] - 1] + Fore.RESET + "| " + Colours[Answer[3] - 1] + Fore.RESET + "|")
+      print("-----------------")
+      print()
+      print()
+      print(Fore.YELLOW + "Codebreaker" + Fore.RESET + " wins - The correct answer was found")
+    print()
+    stop = input("Press Enter To Continue")
+    if(codemaker == 1):
+      codemaker = 2
+    else:
+      codemaker = 1
+  print()
+  print()
+  print(Fore.CYAN + "FINAL SCORES : ")
+  print(user1Name + "'s points > " + str(user1Points))
+  print(user2Name + "'s points > " + str(user2Points))
+  if(user1Points == user2Points):
+    print("It's a " + Fore.YELLOW + "DRAW")
+  elif(user1Points > user2Points):
+    print(Fore.GREEN + user1Name + " WINS")
+  else:
+    print(Fore.GREEN + user2Name + " WINS")
+  print()
+  stop = input("Press Enter To Continue")
+  
+
+
+
     
     
 def Options():
@@ -288,6 +536,8 @@ def Instructions():
 
 
 
+
+
 game = 0
 numberAssist = False
 while(game == 0):
@@ -318,9 +568,10 @@ while(game == 0):
     print()
     print(Fore.CYAN + "MENU :")
     print("1 > Single Player (vs Computer)")
-    print("2 > Instructions")
-    print("3 > Options")
-    print("4 > " + Fore.RED + "Quit")
+    print("2 > Two Player")
+    print("3 > Instructions")
+    print("4 > Options")
+    print("5 > " + Fore.RED + "Quit")
     mode = input("Choice > ")
     print()
     print("--------------------------------------------------")
@@ -330,14 +581,16 @@ while(game == 0):
       SinglePlayer()
     elif(mode == "2"):
       valid = 1
-      Instructions()
+      TwoPlayer()
     elif(mode == "3"):
       valid = 1
-      Options()
+      Instructions()
     elif(mode == "4"):
+      valid = 1
+      Options()
+    elif(mode == "5"):
       valid = 1
       game = 1
       exit = input("Press enter to quit > Thanks for playing! ")
     else:
       print("Invalid Entry > Try again!")
-      
